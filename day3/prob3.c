18782e26ee2467a5010a0c8a9ce3b7981e24f441
hemanth482005 <hemanth.kolkan@gmail.com>
2025-02-19 19:51:29 +0530

    prob3.c

--- day3/prob3.c
+++ day3/prob3.c
@@ -1,37 +1,23 @@
 #include <stdio.h>
-
-void findStats(int scores[], int n) {
-    int highest = scores[0], lowest = scores[0], sum = 0;
-    float average;
-
-    for (int i = 0; i < n; i++) {
-        if (scores[i] > highest) {
-            highest = scores[i];
-        }
-        if (scores[i] < lowest) {
-            lowest = scores[i];
-        }
-        sum += scores[i];
-    }
-
-    average = (float)sum / n;
-
-    printf("Highest Score: %d\n", highest);
-    printf("Lowest Score: %d\n", lowest);
-    printf("Average Score: %.1f\n", average);
-}
-
-int main() {
-    int n;
-    
-    printf("Enter number of matches: ");
-    scanf("%d", &n);
-    
-    int scores[n];
-    printf("Enter scores of %d matches: ", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &scores[i]);
-    }
+ int main() {
+  int scores[5];
+  int highest, lowest, sum = 0;
+  printf("Enter 5 scores: ");
+  for (int i = 0; i < 5; i++) {
+  scanf("%d", &scores[i]);
+  sum += scores[i];
+  if (i == 0) {
+  highest = lowest = scores[i];
+  } else {
+  if (scores[i] > highest) highest = scores[i];
+  if (scores[i] < lowest) lowest = scores[i];
+  }
+  }
+  printf("Highest: %d\n", highest);
+  printf("Lowest: %d\n", lowest);
+  printf("Average: %.1f\n", (float)sum / 5);
+  return 0;
+ }
 
     findStats(scores, n);
 
